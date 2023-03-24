b1005729bf9d3c55f510e382cd29dabc09aa802f
Carl Munene <munenecarl@gmail.com>
2023-03-24 14:48:12 +0300

    Updated the code style

--- Form1.vb
+++ Form1.vb
@@ -2,23 +2,8 @@
     Private Sub BunifuButton1_Click(sender As Object, e As EventArgs) Handles CbnBookAHouse.Click
         Form2.Show()
         Me.Hide()
-
-
     End Sub
-
-
-    Private Sub BunifuButton5_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Panel2_Paint(sender As Object, e As PaintEventArgs) Handles Panel2.Paint
-
-    End Sub
-
-    Private Sub BunifuTextBox5_TextChanged(sender As Object, e As EventArgs) 
-
-    End Sub
-
+    
     Private Sub Cbn_Vacate_a_House_Click(sender As Object, e As EventArgs) Handles Cbn_Vacate_a_House.Click
         Form3.Show()
         Me.Hide()
